0691793120c3e9a939b6adf322ec2d60a8f7bf6e
Rohit Krishnan <krishr.somu@gmail.com>
2025-04-03 17:07:41 +0530

    added initial files

---
+++ main.py
@@ -0,0 +1,10 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+mcp = FastMCP("docs")
+
+def main():
+    print("Hello from documentation!")
+
+
+if __name__ == "__main__":
+    main()
